c2a27bf716ffbe19f3eaa3ca33567334a8705f05
langheran <langheran@gmail.com>
2020-09-23 20:34:20 -0500

    changes

---
+++ disable_adaptive_brightness.bat
@@ -0,0 +1,3 @@
+REM https://www.tenforums.com/tutorials/70157-enable-disable-adaptive-brightness-windows-10-a.html
+powercfg -setdcvalueindex SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 FBD9AA66-9553-4097-BA44-ED6E9D65EAB8 0
+powercfg -setacvalueindex SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 FBD9AA66-9553-4097-BA44-ED6E9D65EAB8 0
